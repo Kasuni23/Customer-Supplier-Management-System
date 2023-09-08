9eb5020efb97982202e3a24e885e5c33951034ce
kasuni23 <kasunimaheshika2325@gmail.com>
2023-09-09 03:03:09 +0530

    complete dashboard UI

--- src/main/java/lk/ijse/customersuppliermanagementsystem/controller/DashboardFormController.java
+++ src/main/java/lk/ijse/customersuppliermanagementsystem/controller/DashboardFormController.java
@@ -1,4 +1,17 @@
 package lk.ijse.customersuppliermanagementsystem.controller;
 
+import javafx.event.ActionEvent;
+
 public class DashboardFormController {
+    public void btnCustomerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnItemOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSupplierOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLogoutOnAction(ActionEvent actionEvent) {
+    }
 }
