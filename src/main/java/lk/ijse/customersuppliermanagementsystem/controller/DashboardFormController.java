5f0411d10efe3ce964d2070b8841edea7f769a75
kasuni23 <kasunimaheshika2325@gmail.com>
2023-09-09 03:16:33 +0530

    complete customer page

--- src/main/java/lk/ijse/customersuppliermanagementsystem/controller/DashboardFormController.java
+++ src/main/java/lk/ijse/customersuppliermanagementsystem/controller/DashboardFormController.java
@@ -1,9 +1,22 @@
 package lk.ijse.customersuppliermanagementsystem.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class DashboardFormController {
-    public void btnCustomerOnAction(ActionEvent actionEvent) {
+    public AnchorPane node;
+    public AnchorPane rootNode;
+
+    public void btnCustomerOnAction(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/customer_form.fxml"));
+
+
+        this.node.getChildren().clear();
+        this.node.getChildren().add(root);
     }
 
     public void btnItemOnAction(ActionEvent actionEvent) {
