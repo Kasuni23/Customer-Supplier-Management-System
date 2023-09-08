6eed12d2b0dd9e364cd3fd229c13ed5ba6240817
kasuni23 <kasunimaheshika2325@gmail.com>
2023-09-09 02:40:00 +0530

    create login page

---
+++ src/main/java/lk/ijse/customersuppliermanagementsystem/controller/DashboardFormController.java
@@ -0,0 +1,4 @@
+package lk.ijse.customersuppliermanagementsystem.controller;
+
+public class DashboardFormController {
+}
---
+++ src/main/java/lk/ijse/customersuppliermanagementsystem/db/db.java
@@ -0,0 +1,6 @@
+package lk.ijse.customersuppliermanagementsystem.db;
+
+public class db {
+    public static final String user = "Kasuni";
+    public static final String pw = "123456";
+}
